e16f715ecdc2a86cbed4be74c2be1100546a9486
PramodaJanaki <137674184+PramodaJanaki@users.noreply.github.com>
2023-07-16 14:35:42 +0530

    Add files via upload

--- FrameAlert.java
+++ FrameAlert.java
@@ -23,12 +23,14 @@ public class FrameAlert {
 		driver.switchTo().frame("dummy-chat-button-iframe");
 		driver.findElement(By.id("dummy-chat-button")).click();
 		driver.switchTo().frame("ShopifyChat");
+		driver.findElement(By.xpath("//textarea[@class='composer-bar__message-text ']")).sendKeys("Hi");
 		driver.findElement(By.xpath("//button[contains(@class,'chat-app--close-button')]")).click();
 		driver.switchTo().defaultContent();
 
 		driver.findElement(By.partialLinkText("URBAN FIT X SMART WATCH")).click();
 		driver.findElement(By.xpath("//div[@class='hxt-spm-clickable']")).click();
 		driver.findElement(By.xpath("//div[@class='wk_zipfinder_input']/input")).sendKeys("600119");
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//div[@class='wk_zipfinder_btn']/input")).click();
 		driver.findElement(By.xpath("//button[@value='Add to Cart']")).click();
 		Thread.sleep(2000);
@@ -36,7 +38,7 @@ public class FrameAlert {
 		driver.findElement(By.xpath("//div[@class='hxt-spm-clickable']")).click();
 		driver.findElement(By.xpath("//input[@id='checkout']")).click();
 		Alert alert = driver.switchTo().alert();
-		System.out.println("Alert message"  +alert.getText());
+		System.out.println("Alert message:"  + alert.getText());
 		alert.accept();
 
 
